a5216a33c8d0830d247699fb7540ea90e096d429
avinash praveen <avinashp.praveen53@gmail.com>
2025-07-31 08:16:00 +0000

    change the description under the title to this 👋 Hello there! I'm Avina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,10 @@ export default function Home() {
                 AVIMDB
               </h1>
               <p className="mx-auto max-w-[700px] text-muted-foreground md:text-xl">
-                Your personal, curated collection of movies, TV shows, and anime. Discover your next favorite.
+                👋 Hello there! I'm Avinash, and this is AVIMDB, my personal vault of some of the finest (and occasionally questionable) movies, TV shows, and anime I’ve consumed over the years.
+                <br /><br />
+                Every title here has been judged by me on factors like acting, music, plot impact, and most importantly... whether I ever felt like watching it again.
+                If your favorite show didn’t make the cut, don’t take it personally it just means your taste is lowkey trash :p
               </p>
             </div>
           </div>
