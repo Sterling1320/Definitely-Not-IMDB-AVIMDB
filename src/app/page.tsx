4309f3a865aebe8b845d9088247f0b1235779277
avinash praveen <avinashp.praveen53@gmail.com>
2025-07-31 10:46:01 +0000

    for anime animation, could you add punching effects like pow pow in comi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,13 @@ export default function Home() {
         <div className="fixed inset-0 z-[200] flex items-center justify-center bg-background/80 animate-page-fade-in">
             {animationType === 'movie' && <Film className="h-32 w-32 text-primary animate-reel-out" />}
             {animationType === 'tv' && <Tv className="h-32 w-32 text-primary animate-tv-out" />}
-            {animationType === 'anime' && <Clapperboard className="h-32 w-32 text-primary animate-sword-slash-out" />}
+            {animationType === 'anime' && (
+              <div className="relative flex items-center justify-center">
+                <Clapperboard className="h-32 w-32 text-primary animate-comic-pow-out" />
+                <span className="absolute -top-8 -left-12 -rotate-12 font-black text-4xl text-yellow-400 stroke-black animate-comic-text" style={{ textShadow: '2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000' }}>POW!</span>
+                <span className="absolute -bottom-4 -right-16 rotate-12 font-black text-5xl text-red-500 animate-comic-text" style={{ animationDelay: '50ms', textShadow: '2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000' }}>POW!</span>
+              </div>
+            )}
             {animationType === 'star' && <Star className="h-32 w-32 text-primary animate-shooting-star-out" />}
         </div>
       )}
