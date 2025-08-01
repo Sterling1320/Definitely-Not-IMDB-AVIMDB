9b8952d0f6c4c98b105b52c9e6a3dc110f2c8de4
avinash praveen <avinashp.praveen53@gmail.com>
2025-08-01 10:21:01 +0000

    Did you remove the number from the cards? Its not visible anymore?

--- src/app/(main)/movies/page.tsx
+++ src/app/(main)/movies/page.tsx
@@ -10,7 +10,7 @@ export default function MoviesPage() {
       </h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6 justify-center">
         {movies.map((movie: Content, index: number) => (
-          <ContentCard key={movie.id} content={movie} />
+          <ContentCard key={movie.id} content={movie} rank={index + 1} />
         ))}
       </div>
     </div>
