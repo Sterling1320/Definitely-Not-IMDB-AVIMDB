807c2d8d432fbb9c10cd2340b0ea57f2a7f3baf9
avinash praveen <avinashp.praveen53@gmail.com>
2025-08-02 07:21:06 +0000

    Change the title of the TV shows section to this 𝔹𝕚𝕟𝕘𝕖 𝕎𝕠𝕣𝕥𝕙�

--- src/app/(main)/tv-shows/page.tsx
+++ src/app/(main)/tv-shows/page.tsx
@@ -7,7 +7,7 @@ export default function TvShowsPage() {
   return (
     <div className="container py-12">
       <div className="text-center">
-        <h1 className="text-4xl font-bold tracking-tight font-headline mb-8">TV Shows</h1>
+        <h1 className="text-4xl font-bold tracking-tight font-headline mb-8">𝔹𝕚𝕟𝕘𝕖 𝕎𝕠𝕣𝕥𝕙𝕪 𝕋𝕍 𝕊𝕙𝕠𝕨𝕤</h1>
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6">
         {tvShows.map((show: Content, index: number) => (
