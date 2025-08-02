596358ebb9966afad4806f720e038aa111a84d15
avinash praveen <avinashp.praveen53@gmail.com>
2025-08-02 07:16:57 +0000

    right now the how it works is I open the site, get the clapper board ani

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
   };
 
 
-  const descriptionText = "𝙷𝚎𝚕𝚕𝚘 𝚃𝚑𝚎𝚛𝚎! 𝙸'𝚖 𝙰𝚟𝚒𝚗𝚊𝚜𝚑 𝚊𝚗𝚍 𝚠𝚎𝚕𝚌𝚘𝚖𝚎 𝚝𝚘 '𝙰𝚅𝙸𝙼𝙳𝙱' — 𝚖𝚢 𝚙𝚎𝚛𝚜𝚘𝚗𝚊𝚕 𝚟𝚊𝚞𝚕𝚝 𝚘𝚏 𝚌𝚒𝚗𝚎𝚖𝚊𝚝𝚒𝚌 𝚝𝚛𝚎𝚊𝚜𝚞𝚛𝚎𝚜 (𝚊𝚗𝚍 𝚝𝚑𝚎 𝚘𝚌𝚌𝚊𝚜𝚒𝚘𝚗𝚊𝚕 𝚐𝚞𝚒𝚕𝚝𝚢 𝚙𝚕𝚎𝚊𝚜𝚞𝚛𝚎).\n\n𝙸 𝚐𝚘𝚝 𝚝𝚒𝚛𝚎𝚍 𝚘𝚏 𝚙𝚎𝚘𝚙𝚕𝚎 𝚊𝚜𝚔𝚒𝚗𝚐 𝚖𝚎 𝚏𝚘𝚛 𝚛𝚎𝚌𝚘𝚖𝚖𝚎𝚗𝚍𝚊𝚝𝚒𝚘𝚗𝚜 𝚊𝚗𝚍 𝚖𝚎 𝚑𝚊𝚟𝚒𝚗𝚐 𝚝𝚘 𝚝𝚑𝚒𝚗𝚔 𝚝𝚘𝚘 𝚖𝚞𝚌𝚑 𝚊𝚋𝚘𝚞𝚝 𝚒𝚝 — 𝚜𝚘 𝚝𝚑𝚒𝚜 𝚒𝚜 𝚖𝚢 𝚜𝚘𝚕𝚞𝚝𝚒𝚘𝚗.\n\n𝙴𝚟𝚎𝚛𝚢 𝚝𝚒𝚝𝚕𝚎 𝚑𝚎𝚛𝚎 𝚒𝚜 𝙰𝚟𝚒𝚗𝚊𝚜𝚑 𝙰𝚙𝚙𝚛𝚘𝚟𝚎𝚍 👍 𝚊𝚗𝚍 𝚝𝚛𝚞𝚜𝚝 𝚖𝚎, 𝚝𝚑𝚎𝚛𝚎'𝚜 𝚜𝚘𝚖𝚎𝚝𝚑𝚒𝚗𝚐 𝚏𝚘𝚛 𝚎𝚟𝚎𝚛𝚢𝚘𝚗𝚎.\n\n𝙸𝚏 𝚢𝚘𝚞𝚛 𝚏𝚊𝚟𝚘𝚛𝚒𝚝𝚎 𝚍𝚒𝚍𝚗'𝚝 𝚖𝚊𝚔𝚎 𝚝𝚑𝚎 𝚕𝚒𝚜𝚝, 𝚒𝚝'𝚜 𝚋𝚎𝚌𝚊𝚞𝚜𝚎 𝚢𝚘𝚞𝚛 𝚝𝚊𝚜𝚝𝚎 𝚒𝚜 𝚜𝚑𝚒𝚝𝚎, respectfully :p\n\n𝙴𝚗𝚓𝚘𝚢 𝚝𝚑𝚎 𝚕𝚒𝚜𝚝.";
+  const descriptionText = "𝙷𝚎𝚕𝚕𝚘 𝚃𝚑𝚎𝚛𝚎! 𝙸'𝚖 𝙰𝚟𝚒𝚗𝚊𝚜𝚑 𝚊𝚗𝚍 𝚠𝚎𝚕𝚌𝚘𝚖𝚎 𝚝𝚘 '𝙰𝚅𝙸𝙼𝙳𝙱' — 𝚖𝚢 𝚙𝚎𝚛𝚜𝚘𝚗𝚊𝚕 𝚟𝚊𝚞𝚕𝚝 𝚘𝚏 𝚌𝚒𝚗𝚎𝚖𝚊𝚝𝚒𝚌 𝚝𝚛𝚎𝚊𝚜𝚞𝚛𝚎𝚜 (𝚊𝚗𝚍 𝚝𝚑𝚎 𝚘𝚌𝚌𝚊𝚜𝚒𝚘𝚗𝚊𝚕 𝚐𝚞𝚒𝚕𝚝𝚢 𝚙𝚕𝚎𝚊𝚜𝚞𝚛𝚎).\n\n𝙸 𝚐𝚘𝚝 𝚝𝚒𝚛𝚎𝚍 𝚘𝚏 𝚙𝚎𝚘𝚙𝚕𝚎 𝚊𝚜𝚔𝚒𝚗𝚐 𝚖𝚎 𝚏𝚘𝚛 𝚛𝚎𝚌𝚘𝚖𝚖𝚎𝚗𝚍𝚊𝚝𝚒𝚘𝚗𝚜 𝚊𝚗𝚍 𝚖𝚎 𝚑𝚊𝚟𝚒𝚗𝚐 𝚝𝚘 𝚝𝚑𝚒𝚗𝚔 𝚝𝚘𝚘 𝚖𝚞𝚌𝚑 𝚊𝚋𝚘𝚞𝚝 𝚒𝚝 — 𝚜𝚘 𝚝𝚑𝚒𝚜 𝚒𝚜 𝚖𝚢 𝚜𝚘𝚕𝚞𝚝𝚒𝚘𝚗.\n\n𝙴𝚟𝚎𝚛𝚢 𝚝𝚒𝚝𝚕𝚎 𝚑𝚎𝚛𝚎 𝚒𝚜 𝙰𝚟𝚒𝚗𝚊𝚜𝚑 𝙰𝚙𝚙𝚛𝚘𝚟𝚎𝚍 👍 𝚊𝚗𝚍 𝚝𝚛𝚞𝚜𝚝 𝚖𝚎, 𝚝𝚑𝚎𝚛𝚎'𝚜 𝚜𝚘𝚖𝚎𝚝𝚑𝚒𝚗𝚐 𝚏𝚘𝚛 𝚎𝚟𝚎𝚛𝚢𝚘𝚗𝚎.\n\n𝙸𝚏 𝚢𝚘𝚞𝚛 𝚏𝚊𝚟𝚘𝚛𝚒𝚝𝚎 𝚍𝚒𝚍𝚗't 𝚖𝚊𝚔𝚎 𝚝𝚑𝚎 𝚕𝚒𝚜𝚝, 𝚒𝚝'𝚜 𝚋𝚎𝚌𝚊𝚞𝚜𝚎 𝚢𝚘𝚞𝚛 𝚝𝚊𝚜𝚝𝚎 𝚒𝚜 𝚜𝚑𝚒𝚝𝚎, respectfully :p\n\n𝙴𝚗𝚓𝚘𝚢 𝚝𝚑𝚎 𝚕𝚒𝚜𝚝.";
   const linkText = "𝙰𝚟𝚒𝚗𝚊𝚜𝚑";
   const linkHref = "https://www.linkedin.com/in/avinash-praveen/";
   
@@ -54,7 +54,7 @@ export default function Home() {
       )}
 
       <div className={cn(
-        "flex flex-col min-h-screen bg-background text-foreground transition-opacity duration-500",
+        "flex flex-col min-h-screen bg-transparent text-foreground transition-opacity duration-500",
         loading ? "opacity-0" : "opacity-100",
         isNavigating && 'animate-page-fade-out'
       )}>
