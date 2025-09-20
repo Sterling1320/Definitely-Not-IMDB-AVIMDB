1a7fe1821487edfe4e901fa0912fa5f3753fc9cf
avinash praveen <avinashp.praveen53@gmail.com>
2025-09-20 13:20:56 +0000

    here add the poster for dept q as well https://images.filmibeat.com/img

--- src/lib/data/tv-shows.ts
+++ src/lib/data/tv-shows.ts
@@ -535,7 +535,7 @@ export const tvShows: Content[] = [
     id: 219,
     title: 'Dept. Q',
     type: 'TV Show',
-    posterUrl: 'https://m.media-amazon.com/images/M/MV5BNTg5Y2VjYjUtYjM4Yi00ZDIyLThjZjYtNzE2ZGFhODFlYzgxXkEyXkFqcGdeQXVyMTAxNzQ1NzI@._V1_.jpg',
+    posterUrl: 'https://images.filmibeat.com/img/popcorn/ws_images/dept.%20q-1786/dept.%20q-1786-s01.jpg',
     aiHint: 'crime thriller',
     shortDescription: 'Follows Carl Mørck, a disgraced detective who is reassigned to the newly created Department Q, a cold case unit.',
     synopsis: 'The series centers on the brilliant but emotionally damaged cold case detective Carl Mørck, who is banished to a basement office and tasked with solving a backlog of cold cases. He and his new assistant, Assad, form an unlikely partnership as they dig into long-forgotten crimes.',
